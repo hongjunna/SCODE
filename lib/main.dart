cf0755b793938d39e876d28d9c9f4656cd2d08b3
imjisu883508 <imjisu883508@gmail.com>
2021-08-25 23:14:17 +0900

    loading page made

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black87,),
+        theme: ThemeData(fontFamily: 'gsans'),
         home: LoadingPage(),
     );
   }
@@ -25,6 +25,47 @@ class LoadingPage extends StatefulWidget {
 class _LoadingPageState extends State<LoadingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      color: Colors.black87,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text("SCODE",
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontFamily: 'gsans',
+              fontSize: 36,
+              color: Colors.white
+            ), textAlign: TextAlign.center,
+          ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 225, 0, 225),
+        ),
+          Text("SQ Code Recommendation\nSystem for Controllers",
+            style: TextStyle(
+                decoration: TextDecoration.none,
+                fontFamily: 'gsans',
+                fontSize: 11,
+                color: Colors.white70
+            ), textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+          ),
+          Text("ⓒ2021 JUNLAB",
+            style: TextStyle(
+                decoration: TextDecoration.none,
+                fontFamily: 'gsans',
+                fontSize: 11,
+                color: Colors.white70
+            ), textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+          ),
+        ],
+      )
+    );
   }
 }
